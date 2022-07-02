24bef16e73948a4aecad73396292f103ffe12933
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2022-07-02 19:49:11 -0300

    General backend updates

--- back/controllers/user_controller.js
+++ back/controllers/user_controller.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const ValidatorContract = require('../validators/fluent-validator')
-const repository = require('../repositories/user_repository')
 
 exports.get = (req, res, next) => {
-    repository.get()
+    User.find({})
     .then(data => {
         res.status(200).send(data)
     }).catch(e => {
@@ -13,7 +12,7 @@ exports.get = (req, res, next) => {
 }
 
 exports.getById = (req, res, next) => {
-    repository.getById(req.params.id)
+    User.findById(req.params.id)
     .then(data => {
         res.status(200).send(data)
     }).catch(e => {
@@ -22,7 +21,24 @@ exports.getById = (req, res, next) => {
 } 
 
 exports.put = (req, res, next) => {
-    repository.update(req.params.id, req.body)
+    User
+    .findByIdAndUpdate(req.params.id, {
+        $set: {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            admin: req.body.admin,
+            CPF: req.body.CPF,
+            number: req.body.number,
+            street: req.body.street,
+            neighborhood: req.body.neighborhood,
+            CEP: req.body.CEP,
+            city: req.body.city,
+            state: req.body.state,
+            telephone: req.body.telephone,
+            cart: req.body.cart
+        }
+    })
     .then (() => {
         res.status(201).send({
             message: "Usuario atualizado com sucesso"
@@ -41,8 +57,9 @@ exports.post = (req, res, next) => {
     contract.isFixedLen(req.body.CPF, 14, 'Digite um CPF válido')
     contract.isFixedLen(req.body.state, 2, 'Digite uma sigla de estado válido')
     contract.isFixedLen(req.body.telephone, 11, 'Digite um número de telefone válido, com DDD, sem parenteses e hífen')
-
-    repository.create(req.body)
+    
+    const user = new User(req.body)
+    user.save()
     .then( () => {
             res.status(201).send({ message: "Usuário cadastrado com sucesso." })
     })
@@ -55,8 +72,7 @@ exports.post = (req, res, next) => {
 }
 
 exports.delete = (req, res, next) => {
-    User.
-    findOneAndRemove(req.params.id)
+    User.findOneAndRemove(req.body.id)
     .then(x => {
         res.status(200).send({
             message: "Usuário removido"
