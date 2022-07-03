efe6ad820ddd93ba9e9abe0317c5931935510e5c
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2022-07-02 22:23:06 -0300

    adding auth

--- back/routes/product-route.js
+++ back/routes/product-route.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/prod_controller')
+const auth = require('../service/auth')
 
 router.get('/', controller.get)
-router.put('/:key', controller.put)
-router.post('/', controller.post)
-router.delete('/:key', controller.delete)
+router.put('/:key', auth.authorize, controller.put)
+router.post('/', auth.authorize, controller.post)
+router.delete('/:key', auth.authorize, controller.delete)
 
 module.exports = router;
--- back/routes/user-route.js
+++ back/routes/user-route.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/user_controller')
+const auth = require('../service/auth')
 
-router.get('/', controller.get)
-router.get('/:id', controller.getById)
-router.put('/:id', controller.put)
+router.post('/auth', controller.authenticate)
+router.get('/', auth.authorize, controller.get)
+router.get('/:id', auth.authorize, controller.getById)
+router.put('/:id', auth.authorize, controller.put)
 router.post('/', controller.post)
-router.delete('/:id', controller.delete)
+router.delete('/:id', auth.authorize, controller.delete)
 
 module.exports = router;
