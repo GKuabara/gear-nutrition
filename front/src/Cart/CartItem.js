0f22be1e3d6da92f1fc9f9f139aa34172b963f77
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2022-07-04 11:35:06 -0300

    fixing cart

--- front/src/Cart/CartItem.js
+++ front/src/Cart/CartItem.js
@@ -16,15 +16,48 @@ const CardItem = ({data, product, user, setUser}) => {
             }
             return item
         })
-        newCart = newCart.filter(item => { return item.qtt != 0 })
-        console.log("Carrinho novo", newCart)
-        setUser({...user, cart: newCart})
+        newCart = newCart.filter(item => { return item.qtt !== 0 })
+
+        // Por algum motivo, quando tiramos o ultimo item do carrinho, o useEffect não era triggerado
+        // Então atualizamos o banco por aqui mesmo
+        if (quantity === 0 && user.cart.length === 1) {
+            const body = {
+                "name": user.name,
+                "email": user.email,
+                "password": user.password,
+                "admin": user.admin,
+                "CPF": user.CPF,
+                "number": user.number,
+                "street": user.street,
+                "neighborhood": user.neighborhood,
+                "CEP": user.CEP,
+                "city": user.city,
+                "state": user.state,
+                "telephone": user.telephone,
+                "cart": [],
+                "token": localStorage.getItem('token')
+            }
+    
+            const url =  "http://localhost:5000/user/" + localStorage.getItem('id')
+            axios.put( url, body )
+            .then(resp => {
+                setQuantity(getCartProd())
+            })
+            .catch(e => {
+                console.log(e)
+            })
+            setUser({...user, cart: []})
+        } else {
+            let newUser = {...user, cart: newCart.filter(item => { return item.qtt !== 0 })}
+            if (user !== newUser) 
+                setUser(newUser)
+        }
     }
     
     function getCartProd() {
         const cartLen = user.cart.length
         for (let i = 0; i < cartLen; i++)
-            if (user.cart[i].id == prod._id) {
+            if (user.cart[i].id === prod._id) {
                 return user.cart[i].qtt;
             }
     }
